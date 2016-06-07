19d01801bacdd7df9a37a5015ca663ce448cb374
Ana Luiza Ferrer <analcferrer@gmail.com>
2016-06-07 15:21:20 -0300

    first commit

---
+++ TrocaComigo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrocaComigo
+//
+//  Created by Ana Luiza Ferrer on 6/7/16.
+//  Copyright © 2016 Ana Luiza Ferrer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
